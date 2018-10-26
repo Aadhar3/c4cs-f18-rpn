040662c215f2cf0a064dae12fe6fca179eb114f8
aadhar <aadhar@umich.edu>
2018-10-26 15:01:47 -0400

    first test passed

--- rpn.py
+++ rpn.py
@@ -2,7 +2,19 @@
 
 
 def calculate(arg):
-    pass
+    stack = []
+
+    tokens = arg.split()
+    for token in tokens:
+        try:
+            stack.append(int(token))
+        except ValueError:
+            val1 = stack.pop()
+            val2 = stack.pop()
+            result = val1 + val2
+
+            stack.append(result)
+    return stack[0]
 
 
 def main():
